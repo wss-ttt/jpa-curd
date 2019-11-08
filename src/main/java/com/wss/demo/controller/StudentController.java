a71cfb726dfdc7d152e2e9fb748900e7dfc7e0ff
王水胜 <602466011@qq.com>
2019-11-08 13:48:23 +0800

    格式化：格式化操作

--- src/main/java/com/wss/demo/controller/StudentController.java
+++ src/main/java/com/wss/demo/controller/StudentController.java
@@ -8,7 +8,9 @@ import org.springframework.web.servlet.ModelAndView;
 import org.thymeleaf.spring5.util.SpringContentTypeUtils;
 
 import javax.annotation.Resource;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Controller
@@ -20,25 +22,27 @@ public class StudentController {
     // 返回所有的数据
     @GetMapping("/list")
     @ResponseBody
-    public List<Student> list(){
+    public List<Student> list() {
         return studentReporitory.findAll();
     }
 
     // 通过id返回单条数据
     @GetMapping("/info/{id}")
     @ResponseBody
-    public  Student info(@PathVariable("id") int id){
+    public Student info(@PathVariable("id") int id) {
         Optional<Student> op = studentReporitory.findById(id);
         Student student = null;
-        if(op.isPresent())
+        if (op.isPresent()) {
             student = op.get();
-        return  student;
+        }
+        return student;
     }
+
     // 通过id进行删除数据
     @PostMapping("/delete/{id}")
     @ResponseBody
-    public boolean delete(@PathVariable("id") int id){
-       Student student = studentReporitory.findById(id).get();
+    public boolean delete(@PathVariable("id") int id) {
+        Student student = studentReporitory.findById(id).get();
         studentReporitory.delete(student);
         return true;
     }
@@ -46,28 +50,38 @@ public class StudentController {
     // 新增数据
     @PostMapping("/add")
     @ResponseBody
-    public boolean add(Student student){
+    public Map<String, String> add(Student student) {
         studentReporitory.save(student);
-        return true;
+        Map<String, String> map = new HashMap<>();
+        map.put("code", "0");
+        map.put("success", "添加成功");
+        return map;
     }
 
     // 修改数据
     @PostMapping("/update")
     @ResponseBody
-    public boolean update(Student s){
+    public Map<String, String> update(Student s) {
         Student student = studentReporitory.findById(s.getId()).get();
+        Map<String, String> map = new HashMap<>();
         student.setAge(s.getAge());
         student.setName(s.getName());
         studentReporitory.save(student);
-        return true;
-    }
-    @GetMapping("/index")
-    public ModelAndView index(){
-        List<Student> lst =studentReporitory.findAll();
-        ModelAndView mav = new ModelAndView("index");
-        mav.addObject("students",lst);
-        return mav;
+        map.put("code", "0");
+        map.put("success", "修改成功");
+        return map;
     }
 
+    @GetMapping("/index")
+    public ModelAndView index() {
+        List<Student> lst = studentReporitory.findAll();
+        ModelAndView mav = new ModelAndView("index");
+        mav.addObject("students", lst);
+        return mav;
+    }
+    @GetMapping("/index2")
+    public String home(){
+       return "index2";
+    }
 
 }
